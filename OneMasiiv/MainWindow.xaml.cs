7b5308c481f281b6d22b9d1c132184b5e5a08fb5
diana <diana@DESKTOP-AE9RS0A>
2020-12-19 22:32:16 +0300

    Добавьте файлы проекта.

---
+++ OneMasiiv/MainWindow.xaml.cs
@@ -0,0 +1,77 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace OneMasiiv
+{
+    /// <summary>
+    /// Логика взаимодействия для MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+        //Function Parse string in int
+        public int[] Parse_String(string str)
+        {
+            return str.Split(' ').Select(x => int.Parse(x)).ToArray();
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            try
+            {
+
+                string num = INPUT.Text;
+                Parse_String(num);
+                string re = " ";
+                int[] nums = Parse_String(num);
+                int[] sum_mas = Parse_String(num); //new massiv summ chisel
+                
+                //MessageBox.Show(nums.ToString());
+                string st = " "; 
+                //vivod massiva d textblock
+                for (int g = 0; g < nums.Length; g++)
+                {
+                    st += nums[g] + " ";
+                }
+
+                for(int d = 0; d < nums.Length; d++)
+                {
+                        //re += nums[d] + nums[j]; Failed
+                        int n1 = nums[d] / 10;
+                        int n2 = nums[d] % 10;
+                        sum_mas[d] = n1 + n2;
+                        re += sum_mas[d] + " ";
+                        
+                }
+                OUTPUT.Text += "Исходный массив: " + st + " " + Environment.NewLine;
+                OUTPUT.Text += "Массив сумм: " + re + " " + Environment.NewLine;
+
+            }
+
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+        }
+
+        private void Button_Click_1(object sender, RoutedEventArgs e)
+        {
+            OUTPUT.Text = String.Empty;
+        }
+    }
+}
